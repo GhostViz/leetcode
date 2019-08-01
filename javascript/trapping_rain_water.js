92a2f5f83431a6c163bd8f46fa0992951469b85f
Joe Nichols <oxhankey@gmail.com>
2019-08-01 17:44:06 -0500

    add comments

--- javascript/trapping_rain_water.js
+++ javascript/trapping_rain_water.js
@@ -1,4 +1,9 @@
-// https://leetcode.com/problems/trapping-rain-water/
+/*
+https://leetcode.com/problems/trapping-rain-water/
+
+Given n non-negative integers representing an elevation
+map where the width of each bar is 1, compute how much water it is able to trap after raining.
+*/
 
 var trap = function(height) {
     let result = 0;
@@ -9,6 +14,14 @@ var trap = function(height) {
     low = 0;
     high = height.length - 1;
 
+    // continue looping while the low is less than or equal to the high
+    // if the height at the low element is less than the height at the high element
+    // check if the low height is greater than left max. If it is make that the new
+    // left max. If it isn't add the max minut the height low to result and increment
+    // low by 1.
+    // if height at the low element is not greater then height at the high element
+    // check if height at the high element is greater than right max. if it is higher
+    // add right max minus the height high to result. Decrement high by 1.
     while (low <= high){
       if (height[low] < height[high]) {
         if (height[low] > leftMax) {
--- javascript/valid_number.js
+++ javascript/valid_number.js
@@ -1,9 +1,22 @@
-# https://leetcode.com/problems/valid-number/
+/*
+https://leetcode.com/problems/valid-number/
 
+Validate if a given string can be interpreted as a decimal number.
+
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one. However, here is a list of characters that can be in a valid decimal number:
+
+    Numbers 0-9
+    Exponent - "e"
+    Positive/negative sign - "+"/"-"
+    Decimal point - "."
+
+Of course, the context of these characters also matters in the input.
+*/
 var isNumber = function(s) {
-    
+    // regex to check positive or negative with or without decimals and e or e-
     let regex = new RegExp(/^[+-]?(([0-9]+\.?[0-9]*)|(\.[0-9]+))([e][+-]?[0-9]+)?$/);
+    // remove extra spaces
     let testString = s.trim();
-    
+    // regex.test returns true or false. 
     return regex.test(testString);
 };
